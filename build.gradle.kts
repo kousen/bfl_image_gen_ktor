33112ddecf6b7bd4f9d68052e57f09ce64876f53
Ken Kousen <ken.kousen@kousenit.com>
2024-10-12 15:31:29 -0400

    Add version catalog and refactor dependencies in build script
    Introduced a new `libs.versions.toml` file to manage library versions more efficiently. Refactored the `build.gradle.kts` to utilize the version catalog for dependencies and plugins, ensuring consistency and ease of updates.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,33 @@
 plugins {
     kotlin("jvm") version "2.0.20"
     kotlin("plugin.serialization") version "2.0.20"
+    alias(libs.plugins.versions)
+    alias(libs.plugins.version.catalog.update)
 }
 
 group = "edu.trincoll"
 version = "1.0-SNAPSHOT"
 
+kotlin {
+    jvmToolchain(17)
+}
+
 repositories {
     mavenCentral()
 }
 
-val ktorVersion = "3.0.0"
-
 dependencies {
-    implementation("io.ktor:ktor-client-core:$ktorVersion")
-    implementation("io.ktor:ktor-client-cio:$ktorVersion")
-    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
-    implementation("io.ktor:ktor-client-logging:$ktorVersion")
-
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.client.logging)
+    implementation(libs.kotlinx.serialization)
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.logback.classic)
     testImplementation(kotlin("test"))
-
-    implementation("ch.qos.logback:logback-classic:1.5.8")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
-kotlin {
-    jvmToolchain(17)
-}
+
